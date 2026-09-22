a681d6dc3d85dbee3f9ea722e913570ef0ff193a
Беляев Роман <belyaev.roman31@gmail.com>
2026-09-22 18:02:22 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@ const SW_VERSION = "w-";
 
 importScripts();
 
-const CACHE = "pogoda-app--m1790081856-m1790082059" + SW_VERSION;
+const CACHE = "radar-win-1790088922" + SW_VERSION;
 const ASSETS = [
   "./index.html",
   "./css/style.css",
